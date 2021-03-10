46010969d5155370bc863e2d2adcd90e8804dc0b
Dunja Acimovic <dunjaacimovicc@gmail.com>
2021-03-10 21:53:29 +0100

    Fixed loop issue

--- MessageQueue.py
+++ MessageQueue.py
@@ -4,8 +4,8 @@ from queue import Empty
 from random import uniform
 
 # Constants
-DIRECTION_ZERO = 3
-DIRECTION_ONE = 3
+DIRECTION_ZERO = 8
+DIRECTION_ONE = 4
 BRIDGE_CAPACITY = 3 
 
 
@@ -37,94 +37,64 @@ def cross_the_bridge(licence_plate, direction_queue, confirm_queue, bridge_queue
     print(licence_plate,"sends -", message)
     bridge_queue.put(message)
 
-    print("------ DONE ------ cross_the_bridge -", licence_plate)
-
 
 def get_cars_to_the_other_side(direction_zero_queue, direction_one_queue, confirm_queue, bridge_queue):
-    direction_is_zero = True
+
     while True:
-        direction_is_zero = not direction_is_zero
-        capacity = [BRIDGE_CAPACITY]
-        # approve_cars(
-        #     capacity=capacity, 
-        #     direction_queue=direction_zero_queue if direction_is_zero else direction_one_queue, 
-        #     confirm_queue=confirm_queue
-        # )
-        start_time = time()
-        direction_time_limit = uniform(0.5, 1.0)
-        while capacity[0] > 0 and (time() - start_time) < direction_time_limit:
-            if handshake(direction_zero_queue if direction_is_zero else direction_one_queue, confirm_queue):
-                capacity[0] -= 1
+        print("\n\n----------------- Direction 1 -------------------")
+        # print("\nDirection 1")
+        capacity = [BRIDGE_CAPACITY, BRIDGE_CAPACITY]
+
+        while direction_one_queue.empty() and capacity[1] > 2:
+            if handshake(direction_one_queue, confirm_queue):
+                capacity[1] = capacity[1] - 1
             else:
                 break
-        simulate_crossing(bridge_queue)
 
-        print("Capacity:", str(capacity[0]))
-        if capacity[0] == BRIDGE_CAPACITY:
-            print("There is no more cars.")
+        while direction_one_queue.empty() and capacity[1] > 1:
+            if handshake(direction_one_queue, confirm_queue):
+                capacity[1] = capacity[1] - 1
+        
+        while direction_one_queue.empty() and capacity[1] > 0:
+            if handshake(direction_one_queue, confirm_queue):
+                capacity[1] = capacity[1] - 1
+        
+        sleep(uniform(1.0, 3.0))
+        if not bridge_queue.empty():
+            print("\nCars which crossed over are: ")
+            while not bridge_queue.empty():
+                message = bridge_queue.get()
+                print("Bridge receives - " + message)
+        else:
+            print("No cars have crossed in direction one.")
+
+        print("\n\n------------------ Direction 0 -------------------")
+        while direction_zero_queue.empty() and capacity[0] > 2:
+            if handshake(direction_zero_queue, confirm_queue):
+                capacity[0] = capacity[0] - 1
+            else:
+                break
+
+        while direction_zero_queue.empty() and capacity[0] > 1:
+            if handshake(direction_zero_queue, confirm_queue):
+                capacity[0] = capacity[0] - 1
+        
+        while direction_zero_queue.empty() and capacity[0] > 0:
+            if handshake(direction_zero_queue, confirm_queue):
+                capacity[0] = capacity[0] - 1
+        
+        sleep(uniform(1.0, 3.0))
+        if not bridge_queue.empty():
+            print("\nCars which crossed over are: ")
+            while not bridge_queue.empty():
+                message = bridge_queue.get()
+                print("Bridge receives - " + message)
+        else:
+            print("No cars have crossed in direction zero.")
+
+        if capacity[0] == BRIDGE_CAPACITY and capacity[1] == BRIDGE_CAPACITY:
+            print("\n------------------------------------------------\n\nTHERE IS NO MORE CARS.\n")
             break
-        
-        print("------ DONE ------ get_cars_to_the_other_side - ITERATION")
-
-    print("------ DONE ------ get_cars_to_the_other_side")
-
-# def get_cars_to_the_other_side(direction_zero_queue, direction_one_queue, confirm_queue, bridge_queue):
-
-#     while True:
-#         capacity = [BRIDGE_CAPACITY]
-
-#         while direction_one_queue.empty() and capacity[0] > 2:
-#             if handshake(direction_one_queue, confirm_queue):
-#                 capacity[0] = capacity[0] - 1
-#             else:
-#                 break
-
-#         while direction_one_queue.empty() and capacity[0] > 1:
-#             if handshake(direction_one_queue, confirm_queue):
-#                 capacity[0] = capacity[0] - 1
-        
-#         while direction_one_queue.empty() and capacity[0] > 0:
-#             if handshake(direction_one_queue, confirm_queue):
-#                 capacity[0] = capacity[0] - 1
-        
-#         sleep(uniform(1.0, 3.0))
-#         print("Cars which crossed over are: ")
-#         while not bridge_queue.empty():
-#             message = bridge_queue.get()
-#             print("Bridge receives - " + message)
-        
-#         print("------ DONE ------ simulate_crossing")
-
-#         capacity = [BRIDGE_CAPACITY]
-
-#         while direction_zero_queue.empty() and capacity[0] > 2:
-#             if handshake(direction_zero_queue, confirm_queue):
-#                 capacity[0] = capacity[0] - 1
-#             else:
-#                 break
-
-#         while direction_zero_queue.empty() and capacity[0] > 1:
-#             if handshake(direction_zero_queue, confirm_queue):
-#                 capacity[0] = capacity[0] - 1
-        
-#         while direction_zero_queue.empty() and capacity[0] > 0:
-#             if handshake(direction_zero_queue, confirm_queue):
-#                 capacity[0] = capacity[0] - 1
-        
-#         sleep(uniform(1.0, 3.0))
-#         print("Cars which crossed over are: ")
-#         while not bridge_queue.empty():
-#             message = bridge_queue.get()
-#             print("Bridge receives - " + message)
-        
-#         print("------ DONE ------ simulate_crossing")
-
-#         if (capacity[0] == BRIDGE_CAPACITY):
-#             print("There is no more cars.")
-#             break
-#         print("------ DONE ------ get_cars_to_the_other_side - ITERATION")
-#         break
-#     print("------ DONE ------ get_cars_to_the_other_side")
         
 
 def approve_cars(capacity, direction_queue, confirm_queue):
@@ -135,36 +105,33 @@ def approve_cars(capacity, direction_queue, confirm_queue):
             capacity[0] -= 1
         else:
             break
-    print("Time's up?", str((time() - start_time) < direction_time_limit))
 
 def handshake(direction_queue, confirm_queue):
-    message = "Bridge: the car is approved to go over."
-    print("Bridge sends -", message)
+    message = "Bridge: a car can go over."
+    print("\nBridge sends -", message)
     direction_queue.put(message)
     try:
-        message = confirm_queue.get(timeout=None)
+        message = confirm_queue.get(timeout=(4.0))
         print("Bridge recieves -", message)
         return True
     except Empty:
+        print("No more cars waiting in direction zero.")
         return False 
         
 def simulate_crossing(bridge_queue):
     sleep(uniform(1.0, 3.0))
-    print("Cars which crossed over are: ")
-    while not bridge_queue.empty():
-        message = bridge_queue.get()
-        print("Bridge receives - " + message)
-    
-    print("------ DONE ------ simulate_crossing")
+    if not bridge_queue.empty():
+        print(bridge_queue.empty())
+        print("Cars which crossed over are: ")
+        while not bridge_queue.empty():
+            message = bridge_queue.get()
+            print("Bridge receives - " + message)
 
 
 def terminate_finished_processes():
-    print("GOT INTO TERMINATE FINISHED")
     for child in active_children():
-        print("GOT IN FOR LOOP", str(child.pid))
         child.join()
         print("Child process", str(child.pid), "joined.")      
-    print("------ DONE ------ terminate_finished_processes")  
 
 
 if __name__ == '__main__':
@@ -178,5 +145,3 @@ if __name__ == '__main__':
 
     get_cars_to_the_other_side(direction_zero_queue, direction_one_queue, confirm_queue, bridge_queue)
     terminate_finished_processes()
-
-    print("Done")
